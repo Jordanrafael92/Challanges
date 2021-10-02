d58e49779606ca9257511d2d4d2e4bc3ef218fc0
Jordan Silva <jordanrafael92@gmail.com>
2021-10-02 14:13:17 -0300

    Configured H2 Database

--- crud/src/main/java/com/jordansilva/crud/repositories/ClientRepository.java
+++ crud/src/main/java/com/jordansilva/crud/repositories/ClientRepository.java
@@ -1,9 +1,11 @@
 package com.jordansilva.crud.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.jordansilva.crud.entities.Client;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
